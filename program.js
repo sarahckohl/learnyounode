7ce6a2107e0e6fba1eefd54b458ebd6eef07eb26
Sarah Kohl <sarahcharlottekohl@gmail.com>
2018-08-17 10:37:20 -0500

    Solve HTTP COLLECT Exercise 8

--- program.js
+++ program.js
@@ -1,13 +1,16 @@
 var http = require('http');
 let earl = process.argv[2];
+let bl = require('bl');
 
 http.get(earl, (response)=>
 {
-    response.setEncoding("utf8");
-    response.on("data", (data) => {
-        console.log(data);
+    response.pipe(bl( (err, data) => {
+        console.log(data.toString().length);
+        console.log(data.toString());
+
     }
-)
+    ));
+            
 }
 )
 
