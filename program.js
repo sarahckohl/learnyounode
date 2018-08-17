7287324e75b41bca821eb11ca0efd5dfabecc1d7
Sarah Kohl <sarahcharlottekohl@gmail.com>
2018-08-17 13:31:34 -0500

    Solve JUGGLING ASYNC Exercise 9

--- program.js
+++ program.js
@@ -1,16 +1,23 @@
-var http = require('http');
-let earl = process.argv[2];
-let bl = require('bl');
+const http = require('http'),
+      bl = require('bl');
 
-http.get(earl, (response)=>
-{
-    response.pipe(bl( (err, data) => {
-        console.log(data.toString().length);
-        console.log(data.toString());
+var queue = [];
+var count = 0;
+http.get(process.argv[2],aux(0));
+http.get(process.argv[3],aux(1));
+http.get(process.argv[4],aux(2));
 
-    }
-    ));
-            
-}
-)
 
+function aux (i){
+    return function callback(response){
+                response.pipe(bl(function (err, data) { 
+                    queue[i] = data.toString();
+                    count++;
+                    if(count == 3){
+                         queue.forEach(function(pos){
+                            console.log(pos);
+                         });
+                    }
+                }));
+            };
+};
